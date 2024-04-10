13529d54869c4ba8f77066504f8586c1fb70c3c5
Danil Volkov <volchara287@gmail.com>
2024-04-10 18:20:44 +0400

    add: добавляет форму которая при успешном логине сохраняет токен в локальное хранилище

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,24 +1,65 @@
 import { Formik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+
+const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(5, 'Минимальное количество символов - 5')
+    .max(30, 'Максимальное количество символов - 30')
+    .required('Обязательное поле'),
+  password: yup
+    .string()
+    .min(5, 'Минимальное количество символов - 5')
+    .max(30, 'Максимальное количество символов - 30')
+    .required('Обязательное поле')
+})
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [errorState, setErrorState] = useState('');
   return (
     <>
       <h1>Hello from LOGIN</h1>
       <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={({ setSubmitting }) => {
-          console.log("Form is validated! Submitting the form...");
-          setSubmitting(false);
+        initialValues={{ username: '', password: '' }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          axios({
+            method: 'post',
+            url: '/api/v1/login',
+            data: {
+              username: values.username,
+              password: values.password,
+            }
+          }).then((res) => {
+            localStorage.setItem('user', JSON.stringify(res.data));
+          }).then(() => {
+            // редирект на главную страницу
+            navigate('/');
+          })
+            .catch((err) => {
+              console.error(err);
+              setErrorState(err.message);
+            })
         }}
       >
-        {() => (
+        {({ errors: validationErrors, touched }) => (
           <Form>
             <div className="form-group">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="username">Username</label>
               <Field
-                type="email"
-                name="email"
+                type="username"
+                name="username"
                 className="form-control"
               />
+              {validationErrors.username && touched.username ? (
+                <div>{validationErrors.username}</div>
+              ) : null}
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
@@ -27,7 +68,13 @@ export default function Login() {
                 name="password"
                 className="form-control"
               />
+              {validationErrors.password && touched.password ? (
+                <div>{validationErrors.password}</div>
+              ) : null}
             </div>
+            {errorState
+              ? (<div>{errorState}</div>)
+              : null}
             <button type="submit">Оправить</button>
           </Form>
         )}
