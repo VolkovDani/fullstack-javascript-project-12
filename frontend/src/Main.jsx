a3420f7d28974f2c1824bc6561a06d301d166b56
Danil Volkov <volchara287@gmail.com>
2024-04-10 18:35:00 +0400

    add: добавляет редирект на страницу логина в случае отсутствия токена

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Main () {
-  return (
-    <>
-      <h1>Hello from MAIN</h1>
-    </>
-  )
+  const navigator = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('user'); 
+    if (!token) navigator('/login')
+    else return (
+      <>
+        <h1>Hello from MAIN</h1>
+      </>
+    )
+  })
 }
