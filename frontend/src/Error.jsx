c044f10e6082232056f06f22712099912e629d80
Danil Volkov <volchara287@gmail.com>
2024-04-11 04:41:35 +0400

    fix: правки замечаний линтёра

--- frontend/src/Error.jsx
+++ frontend/src/Error.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
   return (
     <>
-      <h1 style={{ display: "flex", alignContent: 'center' }}>404</h1>
+      <h1 style={{ display: 'flex', alignContent: 'center' }}>404</h1>
       <Link to='/'>Back to Main</Link>
     </>
-  )
+  );
 }
--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { channels as channelsRoute } from './routes';
@@ -7,21 +7,21 @@ export default function Main() {
   const navigator = useNavigate();
   useEffect(() => {
     const userAuthInfo = JSON.parse(localStorage.getItem('user'));
-    if (!userAuthInfo) navigator('/login')
+    if (!userAuthInfo) navigator('/login');
     else {
       axios.get(channelsRoute.getAll(), {
         headers: {
           Authorization: `Bearer ${userAuthInfo.token}`,
-        }
+        },
       }).then((res) => {
-        console.log(res.data)
+        console.log(res.data);
       })
-        .catch(console.error)
-      return (
-        <>
-          <h1>Hello from MAIN</h1>
-        </>
-      )
+        .catch(console.error);
     }
-  })
+  });
+  return (
+    <>
+      <h1>Hello from MAIN</h1>
+    </>
+  );
 }
