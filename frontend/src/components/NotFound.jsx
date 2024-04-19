c412ec93ddf0ce89ea022c78c607b8c88f5848ff
Danil Volkov <volchara287@gmail.com>
2024-04-19 15:01:27 +0400

    add: добавляет управление текстами при помощи i18n

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-const Error = () => (
-  <>
-    <h1 style={{ display: 'flex', alignContent: 'center' }}>Страница не найдена</h1>
-    <Link to="/">Back to Main</Link>
-  </>
-);
+const NotFound = () => {
+  const { t } = useTranslation('NotFound');
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col pt-3">
+          <h1>{t('pageNotFound')}</h1>
+          <Link to="/">{t('backToMain')}</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Error;
+export default NotFound;
