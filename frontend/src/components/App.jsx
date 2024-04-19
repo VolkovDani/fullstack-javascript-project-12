859de383af1236d1e1c0a89a0ad84760a7ea924a
Danil Volkov <volchara287@gmail.com>
2024-04-19 20:00:02 +0400

    add: компонент Login и его подключение в компонент App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './NotFound.jsx';
+import Login from './Login.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      {/* <Route path="login" element={<Login />} />
-      <Route path="/" element={<Main />} /> */}
+      <Route path="login" element={<Login />} />
+      {/* <Route path="/" element={<Main />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
