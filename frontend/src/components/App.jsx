a3aa5ffd0c58af2eaecfc4e8b898077e7ad2f50d
Danil Volkov <volchara287@gmail.com>
2024-04-19 13:58:01 +0400

    add: начальный App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,3 +1,22 @@
-const App = () => <h1>Hexlet Chat</h1>;
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Main from './Main.jsx';
+import Error from './Error.jsx';
+import Login from './Login.jsx';
+import store from './slices/index';
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
