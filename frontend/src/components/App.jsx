29e76918a15e7a487292644376baf9d5cdda879d
Danil Volkov <volchara287@gmail.com>
2024-04-19 20:11:41 +0400

    add: добавляет роут на главную страницу(где будет находится чат)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './NotFound.jsx';
 import Login from './Login.jsx';
+import Main from './Main.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="login" element={<Login />} />
-      {/* <Route path="/" element={<Main />} /> */}
+      <Route path="/" element={<Main />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
