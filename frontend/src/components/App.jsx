df4091c3b1ef1f30c732972de6566bfcafee8774
Danil Volkov <volchara287@gmail.com>
2024-04-20 11:03:33 +0400

    add: подключение store к App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,18 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import NotFound from './NotFound.jsx';
 import Login from './Login.jsx';
 import Main from './Main.jsx';
+import store from '../slices/index';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/" element={<Main />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
