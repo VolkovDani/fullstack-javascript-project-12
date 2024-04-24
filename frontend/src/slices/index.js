b4b672d6faef75f38fad4df124f51c365eb92ef2
Danil Volkov <volchara287@gmail.com>
2024-04-24 04:32:33 +0400

    add: слайс с сообщениями

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import auth from './auth';
 import channels from './channels';
 import ui from './ui';
+import messages from './messages';
 
 export default configureStore({
   reducer: {
     auth,
     channels,
     ui,
+    messages,
   },
 });
