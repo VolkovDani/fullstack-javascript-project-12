1b3bb99b27c5e8ea16417f277f20b8c0331f5e33
Danil Volkov <volchara287@gmail.com>
2024-05-10 15:53:44 +0400

    add: переписывает кнопку для отправки сообщения

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import leo from 'leo-profanity';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 import { getCurrentChannelId } from '../slices/ui';
 import { messagesSelectors, sendMessage } from '../slices/messages';
@@ -75,33 +78,47 @@ export const InputMessage = () => {
   };
   return (
     <div className="mt-auto px-5 py-3" ref={inputContainerEl}>
-      <form
+      <Form
         className="py-1 border rounded-2"
         onSubmit={handlerSendMessage}
       >
-        <div className="input-group has-validation">
-          <input
+        <InputGroup>
+          <Form.Control
             name="body"
             placeholder={t('InputMessage.enterMessage')}
-            className="border-0 p-0 ps-2 form-control"
+            className="border-0 p-0 ps-2"
             value={value}
+            aria-label={t('InputMessage.aria.enterMessage')}
             onChange={(e) => {
               e.preventDefault();
               setValue(e.target.value);
             }}
-            aria-label={t('InputMessage.aria.enterMessage')}
           />
-          <button
+          <Button
+            variant="vertical"
             type="submit"
-            disabled=""
-            className="btn btn-group-vertical"
+            disabled={!value}
           >
-            <span className={value ? null : 'visually-hidden'}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              width="20"
+              height="20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+              />
+            </svg>
+            <span
+              className="visually-hidden"
+            >
               {t('InputMessage.sendButton')}
             </span>
-          </button>
-        </div>
-      </form>
+          </Button>
+        </InputGroup>
+      </Form>
     </div>
   );
 };
