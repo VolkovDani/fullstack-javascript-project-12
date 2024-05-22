469330d5fe34ea9f01b5fd67fd5f3c036bfbed6f
Danil Volkov <volchara287@gmail.com>
2024-05-23 03:14:38 +0400

    fix: ещё одна правка связанная с получением токена пользователя

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -22,18 +22,20 @@ const Main = () => {
   useEffect(() => {
     if (!authSliceInfo.token) {
       const userAuthInfo = JSON.parse(localStorage.getItem('user'));
-      if (!userAuthInfo) navigator(pagesRoutes.login());
-      dispatch(fetchChannels(userAuthInfo.token))
-        .then((res) => {
-          if (!res.error) {
-            dispatch(authActions.setAuth(userAuthInfo));
-            dispatch(fetchMessages(userAuthInfo.token));
-          } else if (res.error.code === 'ERR_BAD_REQUEST') {
-            navigator(pagesRoutes.login());
-            localStorage.removeItem('user');
-            dispatch(authActions.removeAuth());
-          }
-        });
+      if (!userAuthInfo.token) navigator(pagesRoutes.login());
+      else {
+        dispatch(fetchChannels(userAuthInfo.token))
+          .then((res) => {
+            if (!res.error) {
+              dispatch(authActions.setAuth(userAuthInfo));
+              dispatch(fetchMessages(userAuthInfo.token));
+            } else if (res.error.code === 'ERR_BAD_REQUEST') {
+              navigator(pagesRoutes.login());
+              localStorage.removeItem('user');
+              dispatch(authActions.removeAuth());
+            }
+          });
+      }
     }
   }, [dispatch, navigator, authSliceInfo]);
   const { t } = useTranslation('Components', { keyPrefix: 'Main.Chat' });
